e7c44e94c67b7a91a589f6d65ad71e83410fb016
JarekWnuk <j.p.wnuk@gmail.com>
2023-10-29 11:40:35 +0000

    Adds a method to extract all data in a table and return it as a pandas DataFrame.

--- data_extraction.py
+++ data_extraction.py
@@ -1,29 +1,49 @@
+import pandas as pd
 from sqlalchemy import text
+from database_utils import DatabaseConnector
 
 class DataExtractor:
     def __init__(self) -> None:
         pass
     
-    def query_db(self, query: str):
+    def query_db(self,db_connector_instance: DatabaseConnector, query: str):
         """
-        Establishes an active connection to the database by using the engine returned from the init_db_engine method.
-        Takes in a query that is executed on the connected database.
+        Uses an instance of the DatabaseConnector class to establish a connection to the database.
+        Executes a query on the database and returns the result.
 
         Args:
-            query (str): SQL query to be executed on the database
+            db_connector_instance (DatabaseConnector): an instance of the DatabaseConnector class
+            query (str): a query to be executed on the database
+
         Returns:
-            result: the result of the query, which is an iterable ResultProxy object
+            result (ResultProxy): query result
         """
-        db_engine = self.init_db_engine()
+        db_engine = db_connector_instance.init_db_engine()
         with db_engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
             result = conn.execute(text(query))
         conn.close()
         return result
+    
+    def read_rds_table(self, db_connector_instance: DatabaseConnector, table_name:str) -> pd.DataFrame:
+        """
+        Extracts all data from the passed table (if it exists) and parses to a pandas DataFrame.
 
+        Args:
+            db_connector_instance (DatabaseConnector): an instance of the DatabaseConnector class
+            table_name (str): the name of the table from which to extract data
+        """
+        if table_name in db_connector_instance.list_db_tables():
+            query = f"SELECT * FROM {table_name}"
+            query_result = self.query_db(db_connector_instance, query)
+            df = pd.DataFrame(query_result)
+        else:
+            print("The table specified does not exist.")
+        return df
+
+new_database_conn = DatabaseConnector()
 new_data_extractor = DataExtractor()
-query = "SELECT * FROM orders_table LIMIT 10"
-query_result = new_data_extractor.query_db(query)
-for row in query_result:
-    print(row)
+#print(new_database_conn.list_db_tables())
+table_data_df = new_data_extractor.read_rds_table(new_database_conn, 'legacy_store_details')
+print(table_data_df.head())
 
 
