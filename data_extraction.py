67c66ffba862d462a0ad97d17847438a1e5d0fa4
JarekWnuk <j.p.wnuk@gmail.com>
2023-11-04 11:31:10 +0000

    Adds a method to retrieve data from a pdf file and convert to a pandas dataframe.

--- data_extraction.py
+++ data_extraction.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
+import tabula
 from sqlalchemy import text
 from database_utils import DatabaseConnector
 from data_cleaning import DataCleaning
-from dateutil.parser import parse
 
 class DataExtractor:
     def __init__(self) -> None:
@@ -42,13 +42,30 @@ class DataExtractor:
         else:
             print("The table specified does not exist.")
         return df
+    
+    def retrieve_pdf_data(self, dir:str) -> pd.DataFrame:
+        """
+        Uses the tabula library to extract data from a pdf file to a pandas dataframe.
+        The index is not reset on each page, instead it is continuous.
+
+        Args:
+            dir (str): directory of the pdf file, can be remote
+
+        Returns:
+            concat_df (pd.Dataframe): all pages of the pdf file concatenated to one dataframe
+        """
+        list_of_dfs = tabula.read_pdf(dir, pages='all')
+        concat_df = pd.concat(list_of_dfs, ignore_index=True)
+        return concat_df
+
 
 new_database_conn = DatabaseConnector()
 new_data_extractor = DataExtractor()
 data_cleaning = DataCleaning()
 user_data_df = new_data_extractor.read_rds_table(new_database_conn, 'legacy_users')
 user_data_df_clean = data_cleaning.clean_user_data(user_data_df)
-print(user_data_df_clean.dtypes)
+df_from_pdf = new_data_extractor.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
+print(df_from_pdf.head(70))
 
 
 
